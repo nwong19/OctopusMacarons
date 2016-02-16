dc9797e2ece22037bdf417d11978b2dfe1b0cd17
nwong19 <nwong19@priorypride.net>
2016-02-15 14:53:53 -1000

    TipCalculator
    This is one part of the TipCalculator Project

---
+++ TipCalculatorModel copy.swift
@@ -0,0 +1,43 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Natalie Wong on 11/17/15.
+//  Copyright © 2015 Natalie Wong. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+    
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        
+        let possibleTipsInferred = [0.05, 0.10, 0.15]
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+        
+    }
+    
+}
